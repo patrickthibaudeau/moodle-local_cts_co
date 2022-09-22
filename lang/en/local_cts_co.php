ea3e7618b13a366620f2f5cc82f424dbdde590fa
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-22 19:56:58 -0400

    Added default files

---
+++ lang/en/local_cts_co.php
@@ -0,0 +1,9 @@
+<?php
+$string['client_id'] = 'Client ID';
+$string['client_id_help'] = 'Enter the client ID provided by the API application';
+$string['client_secret'] = 'Client Secret';
+$string['client_secret_help'] = 'Enter the client secret provided by the API application';
+$string['halo_settings'] = 'HALO ITSM Settings';
+$string['tenant'] = 'Tenant';
+$string['tenant_help'] = 'tenant provided by HALO ITSM';
+$string['pluginname'] = 'CTS Computer Order System';
---
+++ settings.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * *************************************************************************
+ * *                   CTS Computer Order System                          **
+ * *************************************************************************
+ * @package     local                                                     **
+ * @subpackage  cts_co                                                    **
+ * @name        CTS Computer Order System                                 **
+ * @copyright   UIT - Innovation lab & EAAS                               **
+ * @link                                                                  **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+defined('MOODLE_INTERNAL') || die;
+
+$systemcontext = context_system::instance();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('local_cts_co', get_string('pluginname', 'local_cts_co'));
+    $ADMIN->add('localplugins',$settings );
+
+    // HALO Settings
+    $settings->add(new admin_setting_heading(
+        'halo_setting',
+        get_string('halo_settings', 'local_cts_co'),
+        ''
+    ));
+    // Halo tenant
+    $settings->add(new admin_setting_configtext(
+        'halo_tenant',
+        get_string('tenant', 'local_cts_co'),
+        get_string('tenant_help', 'local_cts_co'),
+        '',
+        PARAM_TEXT
+    ));
+    // Halo client_id
+    $settings->add(new admin_setting_configtext(
+        'halo_client_id',
+        get_string('client_id', 'local_cts_co'),
+        get_string('client_id_help', 'local_cts_co'),
+        '',
+        PARAM_TEXT
+    ));
+    // Halo client_secret
+    $settings->add(new admin_setting_configpasswordunmask(
+        'halo_client_secret',
+        get_string('client_secret', 'local_cts_co'),
+        get_string('client_secret_help', 'local_cts_co'),
+        '',
+        PARAM_TEXT
+    ));
+
+}
+
+
+
+
+
