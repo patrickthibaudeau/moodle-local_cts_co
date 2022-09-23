355709bed22574aaa10b8066918f4512a45b6de6
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-22 21:50:08 -0400

    Added base class. added new classes for webservice calls

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ global $CFG, $OUTPUT, $SESSION, $PAGE, $DB, $COURSE, $USER;
 require_login(1, false);
 $context = context_system::instance();
 
- \local_cto_co\base::page($CFG->wwwroot . '/local/cto_co/test.php', 'Test', 'Test', $context);
+ \local_cts_co\base::page($CFG->wwwroot . '/local/cto_co/test.php', 'Test', 'Test', $context);
 //**************** ******
 //*** DISPLAY HEADER ***
 //**********************
