068ccbd65762925c214dcced7a74fbb6f5418cba
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-22 22:31:19 -0400

    Testing coneection and results

--- classes/haloitsm.php
+++ classes/haloitsm.php
@@ -26,18 +26,20 @@ class haloitsm extends webservice
 //        $url = $CFG->halo_auth_url;
 //        print_object(http_build_query($data));
 //        // use key 'http' even if you send the request to https://...
-        $options = array(
-            'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method' => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
+//        $options = array(
+//            'http' => array(
+//                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+//                'method' => 'POST',
+//                'content' => http_build_query($data)
+//            )
+//        );
 
         $options = [
             "Content-type: application/x-www-form-urlencoded",
             'Accept: application/form-data'
         ];
+
+        $data = http_build_query($data);
 //        $context = stream_context_create($options);
 //        $result = file_get_contents($url, false, $context);
 //        if ($result === FALSE) {
@@ -51,7 +53,7 @@ class haloitsm extends webservice
         curl_setopt($ch, CURLOPT_URL,$CFG->halo_auth_url);
         curl_setopt($ch, CURLOPT_HTTPHEADER,$options);
         curl_setopt($ch, CURLOPT_POST,1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch, CURLOPT_VERBOSE,true);
         $result = curl_exec ($ch);
