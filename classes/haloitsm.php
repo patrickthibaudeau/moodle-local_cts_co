d35c02a27b2bc2b2188ce950c5fbafc8972b491e
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-22 22:07:41 -0400

    Testing coneection and results

--- classes/haloitsm.php
+++ classes/haloitsm.php
@@ -15,32 +15,66 @@ class haloitsm extends webservice
     {
         global $CFG;
         // Required connection data for HALO AUTH
+//        $data = [
+//            'tenant' => $CFG->halo_tenant,
+//            'grant_type' => 'client_credentials',
+//            'client_id' => $CFG->halo_client_id,
+//            'client_secret' => $CFG->halo_client_secret,
+//            'scope' => 'all'
+//        ];
+//
+//        $url = $CFG->halo_auth_url;
+//        print_object(http_build_query($data));
+//        // use key 'http' even if you send the request to https://...
+//        $options = array(
+//            'http' => array(
+//                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+//                'method' => 'POST',
+//                'content' => http_build_query($data)
+//            )
+//        );
+//        $context = stream_context_create($options);
+//        $result = file_get_contents($url, false, $context);
+//        if ($result === FALSE) {
+//            return false;
+//        } else {
+//            $result_array = json_decode($result);
+//            return $result_array->access_token;
+//        }
+
+
         $data = [
-            'tenant' => $CFG->halo_tenant,
+            'tenant' => 'yorkutest',
             'grant_type' => 'client_credentials',
-            'client_id' => $CFG->halo_client_id,
-            'client_secret' => $CFG->halo_client_secret,
+            'client_id' => '1903f86c-2e4c-4e49-ac7b-8b6b6e0aa54c',
+            'client_secret' => '10392c4c-bf83-4e3f-8349-07e9da700a0f-d317c7cc-99b1-4df0-8e1f-65b34c04a8be',
             'scope' => 'all'
         ];
 
-        $url = $CFG->halo_auth_url;
-        print_object(http_build_query($data));
-        // use key 'http' even if you send the request to https://...
+        $url = 'https://yorkutest.haloitsm.com/auth/token';
+
+
+// use key 'http' even if you send the request to https://...
         $options = array(
             'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method' => 'POST',
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
                 'content' => http_build_query($data)
             )
         );
-        $context = stream_context_create($options);
-        $result = file_get_contents("$url", false, $context);
-        if ($result === FALSE) {
-            return false;
-        } else {
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { /* Handle error */ echo 'Error';} else {
+//echo "<pre>";
             $result_array = json_decode($result);
-            return $result_array->access_token;
+//print_r($result_array);
+//echo "</pre>";
+
+            $token = $result_array->access_token;
+
+            return $token;
         }
+
     }
 
     /**
