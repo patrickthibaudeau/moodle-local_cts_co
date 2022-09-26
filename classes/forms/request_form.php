38c7f48250906ee78979952da5c05228a65f8b73
Patrick Thibaudeau <thibaud@yorku.ca>
2022-09-26 02:35:50 -0400

    Created basic request form

---
+++ classes/forms/request_form.php
@@ -0,0 +1,114 @@
+<?php
+namespace local_cts_co;
+
+use local_cts_co\request;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/lib/formslib.php');
+require_once($CFG->dirroot . '/config.php');
+
+class request_form extends \moodleform
+{
+
+    protected function definition()
+    {
+        global $DB;
+
+        $formdata = $this->_customdata['formdata'];
+        // Create form object
+        $mform = &$this->_form;
+
+        $mform->addElement('hidden', 'id');
+        $mform->setType('id', PARAM_INT);
+
+        //Header: General
+        $mform->addElement('header', 'request_data', get_string('request_header', 'local_cts_co'));
+
+        $mform->addElement('editor', 'description_editor', get_string('description','local_cts_co' ));
+        $mform->addHelpButton('description_editor', 'description', 'local_cts_co');
+        $mform->setType('description_editor', PARAM_RAW);
+
+        $this->add_action_buttons();
+        $this->set_data($formdata);
+    }
+
+    // Perform some extra moodle validation
+    public function validation($data, $files)
+    {
+        global $DB;
+
+        $errors = parent::validation($data, $files);
+
+//        if (is_null($data['id'])) {
+//            $id = -1;
+//        } else {
+//            $id = $data['id'];
+//        }
+//
+//        if ($data['yulearncategoryid'] == 0) {
+//            $errors['yulearncategoryid'] = get_string('field_required', 'local_yulearn');
+//        }
+//
+//        if ($data['id'] < 1) {
+//            $sql = 'SELECT * FROM {yulearn_course} WHERE shortname = "'
+//                . trim($data['shortname']) . '" AND '
+//                . 'id != ' . $id;
+//            if ($foundcourses = $DB->get_records_sql($sql)) {
+//
+//                if (!empty($foundcourses)) {
+//                    foreach ($foundcourses as $foundcourse) {
+//                        $foundcoursenames[] = $foundcourse->fullname;
+//                    }
+//                    $foundcoursenamestring = implode(',', $foundcoursenames);
+//                    $errors['shortname'] = get_string('shortnametaken', '', $foundcoursenamestring);
+//                }
+//            }
+//
+//            if ($foundMoodleCourse = $DB->get_record('course', ['shortname' => trim($data['shortname'])])) {
+//                $errors['shortname'] = get_string('shortnametaken');
+//            }
+//
+//
+//            $sql = 'SELECT * FROM {yulearn_course} WHERE externalcode = "'
+//                . trim($data['externalcode']) . '" AND '
+//                . 'id != ' . $id;
+//            if ($foundcourses = $DB->get_records_sql($sql)) {
+//
+//                if (!empty($foundcourses)) {
+//                    foreach ($foundcourses as $foundcourse) {
+//                        $foundcoursenames[] = $foundcourse->fullname;
+//                    }
+//                    $foundcoursenamestring = implode(',', $foundcoursenames);
+//                    $errors['externalcode'] = get_string('externalcode_taken', 'local_yulearn', $foundcoursenamestring);
+//                }
+//            }
+//
+//            if ($data['hascertificate']) {
+//                if (!$data['certificatetemplateid']) {
+//                    $errors['certificatetemplateid'] = get_string('required', 'local_yulearn');
+//                }
+//            }
+//        }
+//
+//        // Certificate notifications
+//        for ($i = 0; $i < count($data['rnotificationruleid']); $i++) {
+//            if ($data['remailtemplateid'][$i] == 0) {
+//                $errors['remailtemplateid'][$i] = get_string('required', 'local_yulearn');
+//            }
+//        }
+//
+//        for ($i = 0; $i < count($data['remailtemplateid']); $i++) {
+//            if (isset($data['rnotificationruleid'])) {
+//                if ($data['rnotificationruleid'][$i] == 0) {
+//                    $errors['rnotificationruleid'][$i] = get_string('required', 'local_yulearn');
+//                }
+//            } else {
+//                $errors['rnotificationruleid'][$i]  = get_string('required', 'local_yulearn');
+//            }
+//        }
+
+        return $errors;
+    }
+
+}
