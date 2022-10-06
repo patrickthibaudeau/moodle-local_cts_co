2383b163ad554363db7d16a2c7d7a4377a5ad5f1
Patrick Thibaudeau <thibaud@yorku.ca>
2022-10-06 09:04:02 -0400

    Started legend

--- classes/output/details.php
+++ classes/output/details.php
@@ -19,26 +19,29 @@ use local_cts_co\request;
 use local_cts_co\status;
 use local_cts_co\jira;
 
-class details implements \renderable, \templatable {
+class details implements \renderable, \templatable
+{
 
     /**
      * @var int
      */
     private $id;
 
-    public function __construct($id) {
+    public function __construct($id)
+    {
         $this->id = $id;
     }
 
     /**
-     * 
-     * @global type $USER
-     * @global type $CFG
-     * @global \moodle_database $DB
+     *
      * @param \renderer_base $output
      * @return type
+     * @global \moodle_database $DB
+     * @global type $USER
+     * @global type $CFG
      */
-    public function export_for_template(\renderer_base $output) {
+    public function export_for_template(\renderer_base $output)
+    {
         global $USER, $CFG, $DB;
 
         $REQUEST = new request($this->id);
@@ -57,16 +60,109 @@ class details implements \renderable, \templatable {
         $i = 0;
         $timeline = [];
         foreach ($results->statuses as $status) {
-            $timeline[$i]['date'] = strftime(get_string('strftimedatetime'),$status->timecreated);
+            $timeline[$i]['date'] = strftime(get_string('strftimedatetime'), $status->timecreated);
             $timeline[$i]['content'] = $status->status;
             $i++;
+        }
+
+        switch ($i) {
+            case 1:
+                $new_request_completed = false;
+                $quote_process_started = false;
+                $quote_process_not_started = true;
+                $quote_process_completed = false;
+                $order_process_started = false;
+                $order_process_not_started = false;
+                $order_process_completed = false;
+                $receiving_process_started = false;
+                $receiving_process_not_started = false;
+                $receiving_process_completed = false;
+                $order_complete_process_started = false;
+                $order_complete_process_not_started = false;
+                $order_complete_process_completed = false;
+                $inventory_process_started = false;
+                $inventory_process_not_started = false;
+                $inventory_process_completed = false;
+                $imaging_process_started = false;
+                $imaging_process_not_started = false;
+                $imaging_process_completed = false;
+                $setup_process_started = false;
+                $setup_process_not_started = false;
+                $setup_process_completed = false;
+                $pickup_process_started = false;
+                $pickup_process_not_started = false;
+                $pickup_process_completed = false;
+                $deployment_process_started = false;
+                $deployment_process_not_started = false;
+                $deployment_process_completed = false;
+                break;
+            case 2:
+                $new_request_completed = true;
+                $quote_process_started = true;
+                $quote_process_not_started = true;
+                $quote_process_completed = false;
+                $order_process_started = false;
+                $order_process_not_started = true;
+                $order_process_completed = false;
+                $receiving_process_started = false;
+                $receiving_process_not_started = true;
+                $receiving_process_completed = false;
+                $order_complete_process_started = false;
+                $order_complete_process_not_started = true;
+                $order_complete_process_completed = false;
+                $inventory_process_started = false;
+                $inventory_process_not_started = true;
+                $inventory_process_completed = false;
+                $imaging_process_started = false;
+                $imaging_process_not_started = true;
+                $imaging_process_completed = false;
+                $setup_process_started = false;
+                $setup_process_not_started = true;
+                $setup_process_completed = false;
+                $pickup_process_started = false;
+                $pickup_process_not_started = true;
+                $pickup_process_completed = false;
+                $deployment_process_started = false;
+                $deployment_process_not_started = true;
+                $deployment_process_completed = false;
+                break;
+            case 3:
+                $new_request_completed = true;
+                $quote_process_started = true;
+                $quote_process_not_started = false;
+                $quote_process_completed = false;
+                $order_process_started = false;
+                $order_process_not_started = true;
+                $order_process_completed = false;
+                $receiving_process_started = false;
+                $receiving_process_not_started = true;
+                $receiving_process_completed = false;
+                $order_complete_process_started = false;
+                $order_complete_process_not_started = true;
+                $order_complete_process_completed = false;
+                $inventory_process_started = false;
+                $inventory_process_not_started = true;
+                $inventory_process_completed = false;
+                $imaging_process_started = false;
+                $imaging_process_not_started = true;
+                $imaging_process_completed = false;
+                $setup_process_started = false;
+                $setup_process_not_started = true;
+                $setup_process_completed = false;
+                $pickup_process_started = false;
+                $pickup_process_not_started = true;
+                $pickup_process_completed = false;
+                $deployment_process_started = false;
+                $deployment_process_not_started = true;
+                $deployment_process_completed = false;
+                break;
         }
 
         $data = [
             'summary' => $results->request->summary,
             'description' => $results->request->description,
             'halo_ticket_id' => $results->request->halo_ticket_id,
-            'timecreated' => strftime(get_string('strftimedatetime'),$results->request->timecreated),
+            'timecreated' => strftime(get_string('strftimedatetime'), $results->request->timecreated),
             'for_user' => fullname($for_user),
             'by_user' => fullname($by_user),
             'timeline' => json_encode($timeline),
