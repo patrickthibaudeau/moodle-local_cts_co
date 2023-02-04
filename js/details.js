43cbd9defff573d38154d664cd71504a4d851adc
Patrick Thibaudeau <thibaud@yorku.ca>
2023-02-04 17:33:50 -0500

    Changed number of events per slide

--- js/details.js
+++ js/details.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     let events = JSON.parse($('#timeline').val());
     $('#my-timeline').roadmap(events, {
-        eventsPerSlide: 5,
+        eventsPerSlide: 8,
         slide: 1,
         prevArrow: '<i class="fa fa-angle-left"></i>',
         nextArrow: '<i class="fa fa-angle-right"></i>',
