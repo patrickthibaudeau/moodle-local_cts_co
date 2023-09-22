9140b207874bc09df48e8066bc695607f64a5904
Patrick Thibaudeau <thibaud@yorku.ca>
2023-09-22 13:48:01 -0400

    Added key pressed function to submit form on enter

--- js/details.js
+++ js/details.js
@@ -18,7 +18,18 @@ $(document).ready(function () {
 
     $('#get_ticket_details').off();
     $('#get_ticket_details').on('click', function () {
-        let ticket_id = $('#ticket_id').val();
-        window.location.href = wwwroot + "/local/cts_co/status.php?id=" + ticket_id;   //full row of array data
+        search_ticket();
+    });
+
+    $("input").on("keypress", function(event) {
+        if (event.which === 13) {
+            search_ticket();
+        }
     });
 });
+
+function search_ticket() {
+    let wwwroot = M.cfg.wwwroot;
+    let ticket_id = $('#ticket_id').val();
+    window.location.href = wwwroot + "/local/cts_co/status.php?id=" + ticket_id;   //full row of array data
+}
